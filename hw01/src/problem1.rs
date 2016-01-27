2a8331ff473df8e62e8f249dbb8020675aae0590
saml <skynare@gmail.com>
2016-01-26 20:47:33 -0500

    problem1

--- hw01/src/problem1.rs
+++ hw01/src/problem1.rs
@@ -35,3 +35,39 @@ pub fn filter(vs: &Vec<i32>, pred: &Fn(i32) -> bool) -> Vec<i32> {
 
     v
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    // Problem 1
+    //
+
+    // Part 1
+
+    #[test]
+    fn test_sum_small() {
+        let array = [1, 2, 3, 4, 5];
+        assert_eq!(sum(&array), 15);
+    }
+
+    // Part 2
+
+    #[test]
+    fn test_dedup_small() {
+        let vs = vec![1, 2, 2, 3, 4, 1];
+        assert_eq!(dedup(&vs), vec![1, 2, 3, 4]);
+    }
+
+    // Part 3
+
+    fn even_predicate(x: i32) -> bool {
+        (x % 2) == 0
+    }
+
+    #[test]
+    fn test_filter_small() {
+        let vs = vec![1, 2, 3, 4, 5];
+        assert_eq!(filter(&vs, &even_predicate), vec![2, 4]);
+    }
+
+}
